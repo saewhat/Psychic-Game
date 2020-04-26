a08aa13b4113bb3173594779dad7390d03785448
Yoon Ho Ryoo <ryoo94@gmail.com>
2020-04-25 19:12:38 -0700

    made functions that update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,18 @@ var guessesLeft = 9;
 var guesses = [];
 
 var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 // this chooses a random letter within the array above as the computer's choice.
 var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+
+//this updates the 'Guesses Left' counter.
+function updateGuessesLeft() {
+    document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+//this updates 'Your Guesses' list.
+function updateGuesses() {
+    document.querySelector('#guesses').innerHTML = "Your Guesses so far: " + guesses.join(', ');
+}
+
+
 
